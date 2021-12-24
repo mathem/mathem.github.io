08e69fbb5b425e6b7c0c6188d60c2b713c0a608f
volodymyr975 <volodymyryablonskyi@gmail.com>
2021-12-24 10:20:18 +0200

    fisrt

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function viewDiv() {
+    document.getElementById("os").style.display = "block";
+};
+
+function downloadok() {
+    var select = document.getElementById("select");
+    var index = select.value;
+    if (index == 1)
+        window.location = "files/math7win.exe";
+    else if (index == 2) window.location = "files/mathwin.exe";
+   
+}
+function viewLINUX() {
+    document.getElementById("os").style.display = "none";
+};
+function viewMACOS() {
+    document.getElementById("os").style.display = "none";
+};
+function viewform() {
+    document.getElementById("back_form").style.display = "flex";
+};
+function delform() {
+    document.getElementById("back_form").style.display = "none";
+    
+};
+
