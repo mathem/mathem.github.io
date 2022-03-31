7b1b51b01122bd2c6ee1b27d73c081635235f530
mathematic <volodymyryablonskyi@gmail.com>
2022-03-31 13:23:18 +0300

    main

---
+++ js/nav.js
@@ -0,0 +1,17 @@
+
+$(function(){
+    var x=true
+         const navToggle = $ ("#navtoggle");
+          const nav = $ ("#nav");
+        navToggle.on("click", function(event) {
+                           event.preventDefault();
+        nav.toggleClass("show");
+            if (x){   
+            document.getElementById("ui").src='img/clos.svg';
+            } 
+            else{ 
+            document.getElementById("ui").src='img/menu.svg';
+             }
+            x=!x;
+                           });
+            });
