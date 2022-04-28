6c58d362f5addfcc5d62e96d6b7f48a5838acd31
mathematic <volodymyryablonskyi@gmail.com>
2022-04-28 16:21:27 +0300

    main

--- js/down.js
+++ js/down.js
@@ -3,8 +3,7 @@
      document.getElementById("os").style.opacity = 0;
      document.getElementById("os").style.visibility = "hidden";
      document.getElementById("button_1").style.border = "2.5px solid white";
-     document.getElementById("button_2").style.border = "2.5px solid red";
-     document.getElementById("button_3").style.border = "2.5px solid red";
+     
      setTimeout(() => { document.getElementById("os").style.opacity = 1; }, 200);
      setTimeout(() => {  document.getElementById("os").style.visibility = "visible"; }, 200);
      document.getElementById("downclick").onclick = function () {
@@ -17,39 +16,4 @@
 
  };
 
- function viewLINUX() {
-      document.getElementById("os").style.opacity = 0;
-     document.getElementById("os").style.visibility = "hidden";
-     document.getElementById("button_2").style.border = "2.5px solid white";
-     document.getElementById("button_1").style.border = "2.5px solid red";
-     document.getElementById("button_3").style.border = "2.5px solid red";
-     //setTimeout(() => { //document.getElementById("os").style.opacity = 1; }, 200);
-     //setTimeout(() => {  document.getElementById("os").style.visibility = "visible"; }, 200);
-     /*document.getElementById("downclick").onclick = function () {
-         var select = document.getElementById("select");
-         var index = select.value;
-         if (index == 1) {
-             window.location = "files/windows 1.exe";
-         }
-     };*/
- };
-
- function viewMACOS() {
-     document.getElementById("os").style.opacity = 0;
-     document.getElementById("os").style.visibility = "hidden";
-     document.getElementById("button_1").style.border = "2.5px solid red";
-     document.getElementById("button_2").style.border = "2.5px solid red";
-     document.getElementById("button_3").style.border = "2.5px solid white";
-     /*setTimeout(() => { document.getElementById("os").style.opacity = 1; }, 200);
-     setTimeout(() => {  document.getElementById("os").style.visibility = "visible"; }, 200);
-     
-    
-     document.getElementById("downclick").onclick = function () {
-         var select = document.getElementById("select");
-         var index = select.value;
-         if (index == 1) {
-             window.location = "files/linux 1.exe";
-         }
-     };*/
-
- };
+ 
